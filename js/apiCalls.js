79503c8a1cb17faf87beefe4df80c1d2c51fb5ce
Manuel Baleato <span2307@yahoo.com.au>
2014-12-04 23:44:36 +1100

    dunno

--- js/apiCalls.js
+++ js/apiCalls.js
@@ -1,35 +1,62 @@
- 
-document.addEventListener("deviceready", onDeviceReady, false);
+ document.addEventListener("deviceready", onDeviceReady, false);
 
-function onDeviceReady(){
-	console.log(navigator.camera);
-	alert('Device is ready...');
-}
+ function onDeviceReady(){
+ 	console.log(navigator.camera);
+ 	alert('Device is ready!');
+ }
 
+//add code here to handle camera
 function TakePhotoUsingCamera(){
 	TakePhoto(Camera.PictureSourceType.CAMERA);
-
-};
+}
 
 function TakePhotoFromLibrary(){
 	TakePhoto(Camera.PictureSourceType.PHOTOLIBRARY);
-};
+}
 
-function onSuccess(imageData){
+function onCameraSuccess(imageData){
 	var image = document.getElementById('myImage');
 	image.src = "data:image/jpeg;base64," + imageData;
 }
 
-function onFail(message){
+function onCameraFail(message){
 	alert('Failed because: ' + message);
 }
 
 function TakePhoto(sourceType){
-	var camOptions={
-		quality=50,
+	var camOptions = {
+		quality:50,
 		destinationType: Camera.DestinationType.DATA_URL,
 		sourceType: sourceType,
 		correctOrientation: true
 	};
-	navigator.camera.getPicture(onSuccess, onFail, camOptions);
+	navigator.camera.getPicture(onCameraSuccess, onCameraFail, camOptions);
+}
+
+function alertDismissed(){
+	// do nothing
+}
+var onGeoSuccess = function(position){
+	var messageText = 'Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n';
+    navigator.notification.alert(messageText, alertDismissed, 'GPS Location');
+}
+
+function onGeoError(error){
+	    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+}
+
+function ShowGEOLocation(){
+ 	navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);	
+}
+
+function Vibrate(){
+ 	navigator.vibrate(1000);	
 }
